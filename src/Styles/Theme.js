e948a3ddaef04f494af9356d57166142afb904c4
Insoooooooooo <oasisholics@gmail.com>
2020-10-02 11:45:07 +0900

    start develop insootagram front-end

---
+++ src/Styles/Theme.js
@@ -0,0 +1,19 @@
+const BOX_BORDER="1px solid $e5e5e5";
+const BORDER_RADIUS="4px";
+
+export default {
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  blueColor: "#3897f0",
+  darkBlueColor: "#003569",
+  boxBorder: "1px solid #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `${BORDER_RADIUS};
+            ${BOX_BORDER};
+            background-color:white;
+            `,
+  darkGrayColor:"#999",
+  likeGrayColor:"#c7c7c7",
+  redColor:"#ED4956",
+
+};
